68bca188cc52cc35287d8f9803a883c12b0b5773
Elliot Dewhurst <elliot+github@dewhurst.me>
2023-10-05 21:24:41 +0100

    Added key to breadcrumb items

--- src/lib/components/nav/breadcrumb.svelte
+++ src/lib/components/nav/breadcrumb.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
-  import type BreadcrumbItem from "$lib/models/nav/breadcrumb";
+  import type BreadcrumbItem from "$lib/models/nav/breadcrumbItem";
 
   export let items: BreadcrumbItem[] = [];
 
   let lastIndex = items.length - 1;
 </script>
 
-<nav aria-label="breadcrumb" class="flex w-full bg-lime-950 px-4 py-1 text-xs">
-  <ol class="container list-none p-0 mx-auto flex flex-wrap">
-    {#each items as item, index}
-      <li
-        class="mr-1 {index === lastIndex
-          ? 'active'
-          : ''} after:content-['/'] last:after:content-['']"
-      >
-        {#if index === lastIndex}
-          <span>{item.label}</span>
-        {:else}
-          <a href={item.url}>{item.label}</a>
-        {/if}
-      </li>
-    {/each}
-  </ol>
-</nav>
+{#if items}
+  <nav
+    aria-label="breadcrumb"
+    class="flex w-full bg-lime-950 px-4 py-1 text-xs"
+  >
+    <ol class="container list-none p-0 mx-auto flex flex-wrap">
+      {#each items as item, index (item.url)}
+        <li
+          class="mr-1 {index === lastIndex
+            ? 'active'
+            : ''} after:content-['/'] last:after:content-['']"
+        >
+          {#if index === lastIndex}
+            <span>{item.label}</span>
+          {:else}
+            <a href={item.url}>{item.label}</a>
+          {/if}
+        </li>
+      {/each}
+    </ol>
+  </nav>
+{/if}
